def705ae36ee8c29fec8429647371a003ca327f7
Kristopher Ross <thekdr@yahoo.com>
2018-02-01 14:44:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ function inc(n) {
 function dec(n) {
   return (n -= 1)
 }
-makeInt(n) {
+function makeInt(n) {
   return parseInt(n, 10)
+}
+function preserveDecimal(n) {
+  return parseFloat(n)
 }
